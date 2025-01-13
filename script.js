4077f9788fea406e79e40818a22c28ca262d3895
SelimYuksel <yukselselim26@gmail.com>
2025-01-13 14:18:09 +0300

    WIP: Added functionality to operators

--- script.js
+++ script.js
@@ -3,25 +3,28 @@ const equalsBtn = document.querySelector(".btn-equals");
 const clearBtn = document.querySelector(".btn-clear");
 const opBtns = document.querySelectorAll(".btn-op");
 const display = document.querySelector(".display");
+let number1 = 0;
+let number2 = 0;
+let operator = "";
 
 const add = (num1, num2) => num1 + num2;
 const substract = (num1, num2) => num1 - num2;
 const multiply = (num1, num2) => num1 * num2;
 const divide = (num1, num2) => num1 / num2;
+const clearDisplay = () => {
+    display.textContent = "";
+}
 
 const operate = (op, num1, num2) => {
     switch (op) {
         case "+":
-            add(num1, num2);
-            break;
+            return add(num1, num2);
         case "-":
-            substract(num1, num2);
-            break;
+            return substract(num1, num2);
         case "*": 
-            multiply(num1, num2);
-            break;
+            return multiply(num1, num2);
         case "/":
-            divide(num1, num2);
+            return divide(num1, num2);
         default:
             break;
     }
@@ -34,7 +37,28 @@ Array.from(numBtns).forEach(btn => {
 });
 
 clearBtn.addEventListener("click", () => {
-    display.textContent = "";
+    clearDisplay();
 });
+
+Array.from(opBtns).forEach(btn => {
+    btn.addEventListener("click", () => {
+        number1 = Number(display.textContent);
+        operator = btn.value;
+        clearDisplay();
+    });
+});
+
+equalsBtn.addEventListener("click", () => {
+    number2 = Number(display.textContent);
+    let result = operate(operator, number1, number2);
+    display.textContent = result;
+});
+
+
+
+//kullanıcı ilk önce sayı girecek bu sayıyı tutmam lazım
+//sonra + - / * neyse basacak bu operatörü de tutmam lazım 
+//bastıktan sonra bir sayı daha girecek bu sayıyı da tutmam lazım
+//bu tutulan değerlere göre operate fonksiyonunu çağırıcam
 
 
